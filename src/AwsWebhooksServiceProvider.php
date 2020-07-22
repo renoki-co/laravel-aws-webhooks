fead8a90b4e164534d9914194b9ea563fe2c933a
Alex Renoki <rennokki@gmail.com>
2020-07-22 18:40:24 +0300

    wip

--- src/AwsWebhooksServiceProvider.php
+++ src/AwsWebhooksServiceProvider.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace RenokiCo\:package_namespace;
+namespace RenokiCo\AwsWebhooks;
 
 use Illuminate\Support\ServiceProvider;
 
-class :package_service_provider extends ServiceProvider
+class AwsWebhooksServiceProvider extends ServiceProvider
 {
     /**
      * Boot the service provider.
@@ -14,11 +14,11 @@ class :package_service_provider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/../config/config.php' => config_path('config.php'),
+            __DIR__.'/../config/aws-webhooks.php' => config_path('aws-webhooks.php'),
         ]);
 
         $this->mergeConfigFrom(
-            __DIR__.'/../config/config.php', 'config'
+            __DIR__.'/../config/aws-webhooks.php', 'aws-webhooks'
         );
     }
 
