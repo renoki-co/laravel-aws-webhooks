25c3c247a4939efc0e758195cb83854d7667c1fa
Alex Renoki <rennokki@gmail.com>
2020-10-20 00:41:28 +0300

    Refactored the way the methods are being called

--- tests/Controllers/CloudwatchController.php
+++ tests/Controllers/CloudwatchController.php
@@ -20,4 +20,17 @@ class CloudwatchController extends CloudwatchWebhook
             return $request->certificate ?: $url;
         });
     }
+
+    /**
+     * Handle the event when an alarm transitioned to OK.
+     *
+     * @param  array  $message
+     * @param  array  $originalMessage
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     */
+    protected function onOkState(array $message, array $originalMessage, Request $request)
+    {
+        //
+    }
 }
--- tests/SesTest.php
+++ tests/SesTest.php
@@ -6,14 +6,21 @@ use RenokiCo\AwsWebhooks\Http\Controllers\SesWebhook;
 
 class SesTest extends TestCase
 {
-    public function test_callable_methods()
+    public function test_calling_existing_method()
     {
-        foreach (SesWebhook::$eventTypesWithCalledMethod as $callableEventType => $methodToCall) {
-            $payload = $this->getSesMessage($callableEventType);
+        $payload = $this->getSesMessage('Rendering Failure');
 
-            $this->withHeaders($this->getHeadersForMessage($payload))
-                ->json('GET', route('ses', ['certificate' => static::$certificate]), $payload)
-                ->assertSee('OK');
-        }
+        $this->withHeaders($this->getHeadersForMessage($payload))
+            ->json('GET', route('ses', ['certificate' => static::$certificate]), $payload)
+            ->assertSee('OK');
+    }
+
+    public function test_calling_inexisting_method()
+    {
+        $payload = $this->getSesMessage('Bounce');
+
+        $this->withHeaders($this->getHeadersForMessage($payload))
+            ->json('GET', route('ses', ['certificate' => static::$certificate]), $payload)
+            ->assertSee('OK');
     }
 }
